05470354273443638c693821d676be31d32eff1f
Kemp Liao <kempliao@gmail.com>
2022-03-27 22:25:03 -0700

    function added

--- scripts/bookmarks.js
+++ scripts/bookmarks.js
@@ -1,46 +1,90 @@
+//
+//Display bookmarks
+//
 var currentUser;
 firebase.auth().onAuthStateChanged(user => {
     if (user) {
-        currentUser = db.collection("users").doc(user.uid); //global
+        currentUser = db.collection("users").doc(user.uid);
+        db.collection("users").doc(user.uid)
+            .get()
+            .then(function (doc) {
+                var bm = doc.data().bookmarks;
+                //Pass the data to local storage
+                let MeetingsTemplate = document.getElementById("meeting-list-template");
+                console.log(bm);
+                var i;
+                for (i = 0; i < bm.length; i++) {
+                    db.collection("meetings")
+                        // .orderBy("timestamp")
+                        .doc(bm[i])
+                        .get()
+                        .then(doc => {
+                            var meetingID = doc.id;
+                            var title = doc.data().title;
+                            var creator = doc.data().creator;
+                            var date = doc.data().date;
+                            var time = doc.data().time;
+                            var duration = doc.data().duration;
+                            var location = doc.data().location;
+                            var people = doc.data().people;
+                            var description = doc.data().description;
+                            let newcard = MeetingsTemplate.content.cloneNode(true);
+            
+                            //update meetings list
+                            newcard.querySelector('#title').innerHTML = title;
+                            newcard.querySelector('#creator').innerHTML = creator;
+                            newcard.querySelector('#date').innerHTML = date;
+                            newcard.querySelector('#time').innerHTML = time;
+                            newcard.querySelector('#duration').innerHTML = duration;
+            
+                            //update modal description
+                            newcard.querySelector("#modallable").innerHTML = title;
+                            newcard.querySelector("#span1").innerHTML = creator;
+                            newcard.querySelector("#span2").innerHTML = date;
+                            newcard.querySelector("#span3").innerHTML = time;
+                            newcard.querySelector("#span4").innerHTML = duration;
+                            newcard.querySelector("#span5").innerHTML = people;
+                            newcard.querySelector("#span6").innerHTML = location;
+                            newcard.querySelector("#span7").innerHTML = description;
+            
+            
+                            //give unique ids list cards and modals
+                            newcard.querySelector('#brief-list').setAttribute("id", "brief-list" + i);
+                            newcard.querySelector('#detail-modal').setAttribute("id", "detail-modal" + i);
+                            newcard.querySelector('#detailbutton').setAttribute("data-bs-target", "#detail-modal" + i);
+            
+                            //Quit meetings
+                            newcard.querySelector('.quit-button').onclick = () => quit(meetingID);
+                            newcard.querySelector('.quit-button').id = 'quit-' + meetingID;
+            
+            
+                            document.getElementById("meetings-go-here").appendChild(newcard);
+                        })
+            
+                }
+            })
         console.log(currentUser);
-
     } else {
-        // No user is signed in.
         console.log("No user is signed in");
-        window.location.href = "login.html";
     }
 });
 
-function getBookmarks(user) {
-    db.collection("users").doc(user.uid).get()
-        .then(userDoc => {
-            var bookmarks = userDoc.data().bookmarks;
-            console.log(bookmarks);
+displayMeetings("meetings");
 
-            let CardTemplate = document.getElementById("CardTemplate");
-            bookmarks.forEach(thisHikeID => {
-                console.log(thisHikeID);
-                db.collection("Hikes").where("id", "==", thisHikeID).get().then(snap => {
-                    size = snap.size;
-                    queryData = snap.docs;
-
-                    if (size == 1) {
-                        var doc = queryData[0].data();
-                        var hikeName = doc.name; //gets the name field
-                        var hikeID = doc.id; //gets the unique ID field
-                        var hikeLength = doc.length; //gets the length field
-                        let newCard = CardTemplate.content.cloneNode(true);
-                        newCard.querySelector('.card-title').innerHTML = hikeName;
-                        newCard.querySelector('.card-length').innerHTML = hikeLength + " km";
-                        newCard.querySelector('a').onclick = () => setHikeData(hikeID);
-                        newCard.querySelector('img').src = `./images/${hikeID}.png`;
-                        hikeCardGroup.appendChild(newCard);
-                    } else {
-                        console.log("Query has more than one data")
-                    }
-
-                })
-
-            });
-        })
+function quit(meetingID){
+    console.log(meetingID);
+    currentUser.update({
+        meetingsJoined: firebase.firestore.FieldValue.arrayRemove(meetingID)
+    }, {
+        merge: true
+    })
+    .then( () => {
+        console.log("Meeting has been deleted");
+        var buttonID = 'quit-' + meetingID;
+        document.getElementById(buttonID).innerText = "Quit Successfully";
+        document.getElementById(buttonID).className = "btn btn-success quit-button"
+        const timeout = setTimeout(() =>{
+            location.reload()
+        }, 2000);
+    });
 }
--- scripts/meeting.js
+++ scripts/meeting.js
@@ -8,75 +8,66 @@ firebase.auth().onAuthStateChanged(user => {
         db.collection("users").doc(user.uid)
             .get()
             .then(function (doc) {
-                var joinedList = doc.data().meetingsJoined;
+                var jl = doc.data().meetingsJoined;
                 //Pass the data to local storage
-                localStorage.setItem("joinedList", joinedList);
+                let MeetingsTemplate = document.getElementById("meeting-list-template");
+                console.log(jl);
+                var i;
+                for (i = 0; i < jl.length; i++) {
+                    db.collection("meetings")
+                        // .orderBy("timestamp")
+                        .doc(jl[i])
+                        .get()
+                        .then(doc => {
+                            var meetingID = doc.id;
+                            var title = doc.data().title;
+                            var creator = doc.data().creator;
+                            var date = doc.data().date;
+                            var time = doc.data().time;
+                            var duration = doc.data().duration;
+                            var location = doc.data().location;
+                            var people = doc.data().people;
+                            var description = doc.data().description;
+                            let newcard = MeetingsTemplate.content.cloneNode(true);
+            
+                            //update meetings list
+                            newcard.querySelector('#title').innerHTML = title;
+                            newcard.querySelector('#creator').innerHTML = creator;
+                            newcard.querySelector('#date').innerHTML = date;
+                            newcard.querySelector('#time').innerHTML = time;
+                            newcard.querySelector('#duration').innerHTML = duration;
+            
+                            //update modal description
+                            newcard.querySelector("#modallable").innerHTML = title;
+                            newcard.querySelector("#span1").innerHTML = creator;
+                            newcard.querySelector("#span2").innerHTML = date;
+                            newcard.querySelector("#span3").innerHTML = time;
+                            newcard.querySelector("#span4").innerHTML = duration;
+                            newcard.querySelector("#span5").innerHTML = people;
+                            newcard.querySelector("#span6").innerHTML = location;
+                            newcard.querySelector("#span7").innerHTML = description;
+            
+            
+                            //give unique ids list cards and modals
+                            newcard.querySelector('#brief-list').setAttribute("id", "brief-list" + i);
+                            newcard.querySelector('#detail-modal').setAttribute("id", "detail-modal" + i);
+                            newcard.querySelector('#detailbutton').setAttribute("data-bs-target", "#detail-modal" + i);
+            
+                            //Quit meetings
+                            newcard.querySelector('.quit-button').onclick = () => quit(meetingID);
+                            newcard.querySelector('.quit-button').id = 'quit-' + meetingID;
+            
+            
+                            document.getElementById("meetings-go-here").appendChild(newcard);
+                        })
+            
+                }
             })
         console.log(currentUser);
     } else {
         console.log("No user is signed in");
     }
 });
-
-function displayMeetings(collection) {
-    let MeetingsTemplate = document.getElementById("meeting-list-template");
-
-    var jl = localStorage.getItem("joinedList").split(',');
-    console.log(jl);
-    var i;
-    for (i = 0; i < jl.length; i++) {
-
-        db.collection(collection)
-            // .orderBy("timestamp")
-            .doc(jl[i])
-            .get()
-            .then(doc => {
-                var meetingID = doc.id;
-                var title = doc.data().title;
-                var creator = doc.data().creator;
-                var date = doc.data().date;
-                var time = doc.data().time;
-                var duration = doc.data().duration;
-                var location = doc.data().location;
-                var people = doc.data().people;
-                var description = doc.data().description;
-                let newcard = MeetingsTemplate.content.cloneNode(true);
-
-                //update meetings list
-                newcard.querySelector('#title').innerHTML = title;
-                newcard.querySelector('#creator').innerHTML = creator;
-                newcard.querySelector('#date').innerHTML = date;
-                newcard.querySelector('#time').innerHTML = time;
-                newcard.querySelector('#duration').innerHTML = duration;
-
-                //update modal description
-                newcard.querySelector("#modallable").innerHTML = title;
-                newcard.querySelector("#span1").innerHTML = creator;
-                newcard.querySelector("#span2").innerHTML = date;
-                newcard.querySelector("#span3").innerHTML = time;
-                newcard.querySelector("#span4").innerHTML = duration;
-                newcard.querySelector("#span5").innerHTML = people;
-                newcard.querySelector("#span6").innerHTML = location;
-                newcard.querySelector("#span7").innerHTML = description;
-
-
-                //give unique ids list cards and modals
-                newcard.querySelector('#brief-list').setAttribute("id", "brief-list" + i);
-                newcard.querySelector('#detail-modal').setAttribute("id", "detail-modal" + i);
-                newcard.querySelector('#detailbutton').setAttribute("data-bs-target", "#detail-modal" + i);
-
-                //Quit meetings
-                newcard.querySelector('.quit-button').onclick = () => quit(meetingID);
-                newcard.querySelector('.quit-button').id = 'quit-' + meetingID;
-
-
-                document.getElementById(collection + "-go-here").appendChild(newcard);
-            })
-
-    }
-
-
-}
 
 displayMeetings("meetings");
 
@@ -92,5 +83,8 @@ function quit(meetingID){
         var buttonID = 'quit-' + meetingID;
         document.getElementById(buttonID).innerText = "Quit Successfully";
         document.getElementById(buttonID).className = "btn btn-success quit-button"
+        const timeout = setTimeout(() =>{
+            location.reload()
+        }, 2000);
     });
 }
